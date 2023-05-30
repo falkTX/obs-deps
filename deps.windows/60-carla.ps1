5072fbc293d8cd4786ad2ecd4bf19d86e4ace95b
falkTX <falktx@falktx.com>
2023-05-31 00:02:13 +0200

    Try building carla as dep
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ deps.windows/60-carla.ps1
@@ -0,0 +1,64 @@
+param(
+    [string] $Name = 'carla',
+    [string] $Version = '2.6.0-a1',
+    [string] $Uri = 'https://github.com/falkTX/Carla.git',
+    [string] $Hash = '2d1b96372300639ac2059fe083a6d95d97760369'
+)
+
+function Setup {
+    Setup-Dependency -Uri $Uri -Hash $Hash -DestinationPath $Path
+}
+
+function Clean {
+    Set-Location $Path
+
+    if ( Test-Path "build_${Target}" ) {
+        Log-Information "Clean build directory (${Target})"
+        Remove-Item -Path "build_${Target}" -Recurse -Force
+    }
+}
+
+function Configure {
+    Log-Information "Configure (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        $CmakeOptions
+        '-DCARLA_USE_JACK=OFF'
+        '-DCARLA_USE_OSC=OFF'
+    )
+
+    Invoke-External cmake -S cmake -B "build_${Target}" @Options
+}
+
+function Build {
+    Log-Information "Build (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--build', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $VerbosePreference -eq 'Continue' ) {
+        $Options += '--verbose'
+    }
+
+    Invoke-External cmake @Options
+}
+
+function Install {
+    Log-Information "Install (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--install', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $Configuration -match "(Release|MinSizeRel)" ) {
+        $Options += '--strip'
+    }
+
+    Invoke-External cmake @Options
+}

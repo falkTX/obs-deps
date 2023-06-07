7b8534cfa9988d1bca07446a65348d528b609b61
falkTX <falktx@falktx.com>
2023-06-07 07:40:20 +0200

    Add carla as dependency to macOS and Windows builds
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ deps.windows/60-carla.ps1
@@ -0,0 +1,64 @@
+param(
+    [string] $Name = 'carla',
+    [string] $Version = '2.6.0-alpha1',
+    [string] $Uri = 'https://github.com/falkTX/Carla.git',
+    [string] $Hash = '051c15680936c4b72a7d41e1edbee2b47741553c'
+)
+
+function Setup {
+    Setup-Dependency -Uri $Uri -Hash $Hash -DestinationPath $Path
+}
+
+function Clean {
+    Set-Location $Path
+
+    if ( Test-Path "build_${Target}" ) {
+        Log-Information "Clean build directory (${Target})"
+        Remove-Item -Path "build_${Target}" -Recurse -Force
+    }
+}
+
+function Configure {
+    Log-Information "Configure (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        $CmakeOptions
+        '-DCARLA_USE_JACK:BOOL=OFF'
+        '-DCARLA_USE_OSC:BOOL=OFF'
+    )
+
+    Invoke-External cmake -S cmake -B "build_${Target}" @Options
+}
+
+function Build {
+    Log-Information "Build (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--build', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $VerbosePreference -eq 'Continue' ) {
+        $Options += '--verbose'
+    }
+
+    Invoke-External cmake @Options
+}
+
+function Install {
+    Log-Information "Install (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--install', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $Configuration -match "(Release|MinSizeRel)" ) {
+        $Options += '--strip'
+    }
+
+    Invoke-External cmake @Options
+}
